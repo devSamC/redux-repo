15613d6656ff70288ef8c06a0ab8068cf645e8fa
brodie-m <brodiedmcguire@gmail.com>
2021-10-14 16:16:44 +0100

    making store

--- redux-assignment/src/app/store.js
+++ redux-assignment/src/app/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
-});
+},composeWithDevTools(applyMiddleware(thunk)));
